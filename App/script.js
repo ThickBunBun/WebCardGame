94838a2182a20c4d32920167d5464a4670335bae
thickbunbun <thickbunbun@tutanota.com>
2023-12-13 18:00:39 +0200

    game logic, card generation, timer, reset button

--- App/script.js
+++ App/script.js
@@ -1,15 +1,149 @@
-// function countDown() {
-//   let curval = document.getElementById('countdown').innerText
-//   if (curval > 0) {
-//     document.getElementById('countdown').innerHTML = Number(curval) - 1
-//   }
-// }
-// reveals front of the crad and shows back and viceversa
-
+//game logic vars
 var valuesofcards = [];
-var idofcards = []
+var idofcards = [];
+var score = 0;
+var paircount = 0;
 
 
+//card shuffling
+function shuffle(arr) {
+  let currentIndex = arr.length;
+  let randomIndex = 0;
+
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
+  };
+
+  return arr;
+};
+
+
+function htmlElemGen(html) {
+  const template = document.createElement('template');
+
+  template.innerHTML = html.trim();
+
+  return template.content.firstElementChild;
+};
+
+//card generation
+function cardGen() {
+  document.getElementById('cardfield').innerHTML = ''
+
+  card = htmlElemGen(`
+        <div class="cardinstance" id="crd_#" onclick="reveal(this.id)">
+          <div class="card">
+            <div class="cardhead"></div>
+            <div class="clickbody">
+              <p class="cardback">:3</p>
+              <p class="cardface">#</p>
+            </div>
+          </div>
+        </div>
+  `);
+
+  let card_content = [];
+
+  for (let index = 0; index <= 7; index++) {
+    card_content.push(Math.floor(Math.random() * 20) + 1);
+  }
+
+  card_content = card_content.concat(card_content);
+  card_content = shuffle(card_content)
+
+  console.log(card_content);
+
+  console.log(document.getElementById('cardfield'))
+
+  for (let index = 0; index <= 15; index++) {
+    // console.log(item);
+    let card = htmlElemGen(`
+      <div class="cardinstance" id="crd_${index}" onclick="reveal(this.id)">
+        <div class="card">
+          <div class="cardhead"></div>
+          <div class="clickbody">
+            <p class="cardback">:3</p>
+            <p class="cardface">${card_content[index]}</p>
+          </div>
+        </div>
+      </div>
+    `);
+
+    // console.log(index)
+    document.getElementById('cardfield').appendChild(card);
+  };
+};
+
+// function show() {
+//   // document.getElementsByClassName("cardinstance").getElementsByClassName("cardface").style.display = "block"
+//   // document.getElementsByClassName("cardinstance").getElementsByClassName("cardback").style.display = "block"
+//   console.log('!');
+//   let cardfaces = document.querySelectorAll('.cardface');
+//   let cardbacks = document.querySelectorAll('.cardback');
+//   cardfaces.forEach(el => {
+//     el.style.display = 'block';
+//   });
+//   cardbacks.forEach(el => {
+//     el.style.display = 'none';
+//   });
+//   setTimeout(() => {
+//     cardfaces.forEach(el => {
+//       el.style.display = 'none';
+//     });
+//     cardbacks.forEach(el => {
+//       el.style.display = 'block';
+//     });
+//   }, 500);
+// };
+
+
+// refreshes the field
+function refresh() {
+  setTimeout(() => {
+    cardGen();
+  }, 500);
+};
+
+
+//reset button func
+function reset() {
+  cardGen();
+  score = 0;
+  paircount = 0;
+  gameseconds = gametime * 60;
+  document.getElementById('score_num').innerText = score;
+};
+
+
+//game timer
+const gametime = 2;
+let gameseconds = gametime * 60;
+const timerEl = document.getElementById('timer');
+
+function timer() {
+  let minutes = Math.floor(gameseconds / 60);
+  let seconds = gameseconds % 60;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  if (gameseconds > 0) {
+    gameseconds--;
+  };
+  timerEl.innerHTML = `${minutes}:${seconds}`;
+};
+
+
+function startTimer(start = true) {
+  if (start == true) {
+    setInterval(timer, 1000);
+  }
+};
+
+
+startTimer()
+
+
+// gamelogic
 function reveal(id) {
   // declaring elemet links
   const cardface = document.getElementById(id).getElementsByClassName("cardface");
@@ -23,6 +157,7 @@ function reveal(id) {
       valuesofcards.push(cardface[0].innerText);
       idofcards.push(document.getElementById(id).id);
     };
+    //checking the amoung of cards that've been clicked, if 2, compare the values 
     if (valuesofcards.length == 2) {
       if (valuesofcards[0] === valuesofcards[1]) {
         document.getElementById(idofcards[0]).style.border = "green 5px solid";
@@ -31,6 +166,13 @@ function reveal(id) {
         document.getElementById(idofcards[1]).getElementsByClassName("cardhead")[0].style.backgroundColor = "green";
         idofcards = [];
         valuesofcards = [];
+        score++;
+        paircount++;
+        console.log(paircount)
+        if (paircount == 8) {
+          refresh()
+        }
+        document.getElementById('score_num').innerText = score;
       }
       else {
         setTimeout(() => {
@@ -44,13 +186,15 @@ function reveal(id) {
 
       };
     };
+    //debuggin section
     console.log(valuesofcards);
-    console.log(idofcards)
+    console.log(idofcards);
     return cardface.innerText;
-  }
+  };
   // else {
   //   cardback[0].style.display = 'block';
   //   cardface[0].style.display = 'none';
   // };
 };
+
 
